258a18e94da0d140890cf29edf6a7af99652c541
DreadCorsair <igorbarulin@gmail.com>
2015-09-05 23:45:23 +0300

    btn start and quit on start menu handled

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class UIManager : MonoBehaviour 
+{
+	public void StartGame()
+	{
+		Application.LoadLevel("Game");
+	}
+
+	public void QuitGame()
+	{
+		Application.Quit();
+	}
+}
