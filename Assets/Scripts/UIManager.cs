2754d37aba13254e5defd39249a80bc8bf721a9b
DreadCorsair <igorbarulin@gmail.com>
2015-09-06 15:20:14 +0300

    score handled

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,7 +1,20 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour 
 {
+	public Text Score;
+
+	private void Start()
+	{
+		Score.text = Referee.Score.ToString();
+	}
+
+	private void Update()
+	{
+		Score.text = Referee.Score.ToString();
+	}
+
 	public void StartGame()
 	{
 		Application.LoadLevel("Game");
